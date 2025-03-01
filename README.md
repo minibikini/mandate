f45e0a3f2b78d4f9bf044fe6d5be14e137c63452
Egor Kislitsyn <egor@kislitsyn.com>
2025-03-01 04:55:41 +0400

    docs: Improve README and mix.exs with project details

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # Mandate
 
-**TODO: Add description**
+Build robust, elegant CLIs with minimal boilerplate.
 
 ## Installation
 
-If [available in Hex](https://hex.pm/docs/publish), the package can be installed
-by adding `mandate` to your list of dependencies in `mix.exs`:
+The package can be installed by adding `mandate` to your list of dependencies in `mix.exs`:
 
 ```elixir
 def deps do
@@ -14,8 +13,3 @@ def deps do
   ]
 end
 ```
-
-Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
-and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
-be found at <https://hexdocs.pm/mandate>.
-
--- mix.exs
+++ mix.exs
@@ -3,12 +3,20 @@ defmodule Mandate.MixProject do
 
   def project do
     [
+      name: "Mandate",
+      description: description(),
+      package: package(),
       app: :mandate,
       version: "0.1.0",
       elixir: "~> 1.18",
+      source_url: "https://github.com/minibikini/mandate",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
+  end
+
+  defp description do
+    "Build robust, elegant CLIs with minimal boilerplate"
   end
 
   # Run "mix help compile.app" to learn about applications.
@@ -24,6 +32,16 @@ defmodule Mandate.MixProject do
       {:sourceror, "~> 1.7", only: [:dev, :test]},
       {:spark, "~> 2.2"},
       {:igniter, "~> 0.5", only: [:dev, :test]}
+    ]
+  end
+
+  defp package() do
+    [
+      # These are the default files included in the package
+      files: ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE*
+                  license* CHANGELOG* changelog* src),
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/minibikini/mandate"}
     ]
   end
 end
