80f7865c1e137dbc4a9d65724c791043ed00c426
Egor Kislitsyn <egor@kislitsyn.com>
2025-03-02 23:21:24 +0400

    feat: Add longdoc attribute to module and debug dsl_state

--- lib/mandate/transformers/add_doc_attributes.ex
+++ lib/mandate/transformers/add_doc_attributes.ex
@@ -10,22 +10,23 @@ defmodule Mandate.Transformers.AddDocAttributes do
       |> Enum.find(fn x -> is_struct(x, Mandate.Dsl.Shortdoc) end)
       |> Map.get(:shortdoc)
 
-    # longdoc =
-    #   dsl_state
-    #   |> Map.get([:root])
-    #   |> Map.get(:entities)
-    #   |> Enum.find(fn x -> is_struct(x, Mandate.Dsl.Longdoc) end)
-    #   |> Map.get(:longdoc)
+    longdoc =
+      dsl_state
+      |> Map.get([:root])
+      |> Map.get(:entities)
+      |> Enum.find(fn x -> is_struct(x, Mandate.Dsl.Longdoc) end)
+      |> Map.get(:longdoc)
 
-    # %{moduledoc: {0, longdoc}, shortdoc: shortdoc}
     dsl_state =
-      %{shortdoc: shortdoc}
+      %{moduledoc: {1, longdoc}, shortdoc: shortdoc}
       |> Map.reject(fn {_k, v} -> is_nil(v) end)
+      |> dbg()
       |> Enum.reduce(dsl_state, fn {key, value}, dsl_state ->
         Spark.Dsl.Transformer.eval(
           dsl_state,
           [key: key, value: value],
           quote do
+            Module.register_attribute(__MODULE__, unquote(key), persist: true)
             Module.put_attribute(__MODULE__, unquote(key), unquote(value))
           end
         )
