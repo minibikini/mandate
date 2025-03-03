e9baa59b4c9e3e08827b733252fabd165136d7a2
Egor Kislitsyn <egor@kislitsyn.com>
2025-03-03 04:46:38 +0400

    test: Improve error message tests for mix task generator

--- test/mix/tasks/mandate.gen.mix_task_test.exs
+++ test/mix/tasks/mandate.gen.mix_task_test.exs
@@ -1,13 +1,20 @@
 defmodule Mix.Tasks.Mandate.Gen.MixTaskTest do
   use ExUnit.Case, async: true
   alias Mix.Tasks.Mandate.Gen.MixTask
+  import ExUnit.CaptureIO
 
   @task_name "mandate.gen.mix_task"
+
+  test "is a mix task" do
+    assert Mix.Task.task?(MixTask)
+    assert @task_name == Mix.Task.task_name(MixTask)
+  end
 
   test "inject @shortdoc" do
     attributes = MixTask.__info__(:attributes)
 
-    assert ["Generates a new Mix Task"] = Keyword.get(attributes, :shortdoc)
+    assert ["Generates a new Mix Task"] == Keyword.get(attributes, :shortdoc)
+    assert "Generates a new Mix Task" == Mix.Task.shortdoc(MixTask)
   end
 
   test "inject @moduledoc" do
@@ -22,20 +29,22 @@ defmodule Mix.Tasks.Mandate.Gen.MixTaskTest do
       Mix.Task.reenable(@task_name)
     end
 
-    test "xxxx" do
-      msg = "Too many arguments. Expected maximum 1 but got 4."
+    test "Too many arguments" do
+      output =
+        capture_io(:stderr, fn ->
+          Mix.Task.run(@task_name, ["one", "s", "sdds", "sds"])
+        end)
 
-      assert_raise RuntimeError, msg, fn ->
-        Mix.Task.run(@task_name, ["one", "s", "sdds", "sds"])
-      end
+      assert output =~ "Too many arguments. Expected maximum 1 but got 4."
     end
 
     test "Required arguments presence." do
-      msg = "Wrong number of required arguments. Expected 1 but got 0."
+      output =
+        capture_io(:stderr, fn ->
+          Mix.Task.run(@task_name, [])
+        end)
 
-      assert_raise RuntimeError, msg, fn ->
-        Mix.Task.run(@task_name, [])
-      end
+      assert output =~ "Wrong number of required arguments. Expected 1 but got 0."
     end
   end
 end
