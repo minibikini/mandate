00dc603f9901fe529dfdc172d402367fb7228ad5
Egor Kislitsyn <egor@kislitsyn.com>
2025-03-04 07:33:24 +0400

    feat: Add shortdoc and longdoc attributes to the root section

--- lib/mandate/dsl.ex
+++ lib/mandate/dsl.ex
@@ -1,24 +1,9 @@
 defmodule Mandate.Dsl do
   @moduledoc false
   @root %Spark.Dsl.Section{
-    # auto_set_fields: term(),
-    # deprecations: term(),
-    # describe: term(),
-    # docs: term(),
-    # entities: term(),
-    # examples: term(),
-    # imports: term(),
-    # links: term(),
-    # modules: term(),
-    # name: term(),
-    # no_depend_modules: term(),
-    # patchable?: term(),
-    # schema: term(),
-    # sections: term(),
-    # snippet: term(),
-    # top_level?: term()
-
     name: :root,
+    describe: "The root section",
+    top_level?: true,
     schema: [
       run: [
         # type: {:one_of, [{:fun, 1}, {:fun, 2}]},
@@ -34,22 +19,22 @@ defmodule Mandate.Dsl do
 
         end
         """
+      ],
+      shortdoc: [
+        type: :string
+      ],
+      longdoc: [
+        type: :string
       ]
     ],
     entities: [
       Mandate.Dsl.Argument.__entity__(),
-      Mandate.Dsl.Description.__entity__(),
-      Mandate.Dsl.Longdoc.__entity__(),
-      # Mandate.Dsl.Run.__entity__(),
-      Mandate.Dsl.Shortdoc.__entity__(),
       Mandate.Dsl.Switch.__entity__()
-    ],
-    describe: "Configure the fields that are supported and required",
-    top_level?: true
+    ]
   }
 
   use Spark.Dsl.Extension,
     sections: [@root],
-    verifiers: [Spark.Dsl.Verifiers.VerifyEntityUniqueness],
-    transformers: [Mandate.Transformers.AddDocAttributes]
+    transformers: [Mandate.Transformers.AddDocAttributes],
+    verifiers: [Spark.Dsl.Verifiers.VerifyEntityUniqueness]
 end
--- lib/mandate/mix_task.ex
+++ lib/mandate/mix_task.ex
@@ -14,18 +14,6 @@ defmodule Mandate.MixTask do
           {:error, err} -> Mix.shell().error(err)
         end
       end
-
-      # case Mandate.OptionParser.parse_argv(root, argv) do
-      #   {:ok, parsed} -> Mandate.Info.root_run!(__MODULE__, parsed)
-      #   {:error, err} -> Mix.shell().error(err)
-      # end
     end
   end
 end
-
-# def run(root, parsed_argv) do
-#   root
-#   |> Enum.find(&is_struct(&1, Mandate.Dsl.Run))
-#   |> then(fn run -> apply(run.fun, [parsed_argv]) end)
-# end
-# end
