14141a4caa00433e5517aa86783e275caa7f1028
Egor Kislitsyn <egor@kislitsyn.com>
2025-03-07 04:49:14 +0400

    refactor: Refactor mandate DSL to use spark DSL sections

--- lib/mandate/dsl/commands.ex
+++ lib/mandate/dsl/commands.ex
@@ -1,22 +1,22 @@
-defmodule Mandate.Dsl.Commands do
-  @moduledoc false
-  defstruct [:__identifier__, :name, :type, :required, :default, :short, :keep, :doc]
+# defmodule Mandate.Dsl.Commands do
+#   @moduledoc false
+#   defstruct [:__identifier__, :name, :type, :required, :default, :short, :keep, :doc]
 
-  @doc false
-  def __entity__,
-    do: %Spark.Dsl.Section{
-      name: :commands,
-      describe: "A collection of commands",
-      # args: [:name, {:optional, :type}],
-      # identifier: :name,
-      # target: Mandate.Dsl.Commands,
-      schema: [
-        name: [
-          type: :atom
-        ]
-      ],
-      entities: [
-        Mandate.Dsl.Command.__entity__()
-      ]
-    }
-end
+#   @doc false
+#   def __entity__,
+#     do: %Spark.Dsl.Section{
+#       name: :commands,
+#       describe: "A collection of commands",
+#       # args: [:name, {:optional, :type}],
+#       # identifier: :name,
+#       # target: Mandate.Dsl.Commands,
+#       schema: [
+#         name: [
+#           type: :atom
+#         ]
+#       ],
+#       entities: [
+#         Mandate.Dsl.Command.__entity__()
+#       ]
+#     }
+# end
